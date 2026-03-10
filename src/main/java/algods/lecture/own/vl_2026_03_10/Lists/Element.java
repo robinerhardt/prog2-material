3d3b5d8234a8b7b79d4feb1df5f5add65144d3b2
Robin Erhardt <67264436+RobinErhardt@users.noreply.github.com>
2026-03-10 11:48:32 +0100

    Element own solution added

---
+++ src/main/java/algods/lecture/own/vl_2026_03_10/Lists/Element.java
@@ -0,0 +1,54 @@
+package algods.lecture.own.vl_2026_03_10.Lists;
+
+public class Element {
+    int data;
+    Element next;
+    Element prev;
+
+    /// Standardkonstruktor. Erzeugt ein neues leeres Element.
+    /// 
+    /// Anmerkung:
+    /// - Die Aufgabe des Konstruktors ist, ein neues Element zu erzeugen und dabei
+    ///   die Rahmenbedingen herzustellen, unter denen das Element funktioniert.
+    /// - In diesem Fall muss die Listenstruktur (also Verknüpfung zu Vorgänger und
+    ///   Nachfolger) hergestellt werden.
+    public Element() {
+        // 1. Schritt: Die Umgebung erzeug eine Variable
+        // `this`, die auf den Speicher zeigt, der gerade
+        // für das neue `Element` reserviert wurde.
+
+        // Wir initialisieren die `next` und `prev`-Felder
+        // von this.
+        this.next = this;
+        this.prev = this;
+
+        // Folgendes wird am Ende der Funktion implizit passieren:
+        // return this;
+    }
+
+    /// Hängt ein Element ans Ende der Liste an.
+    public void add(int d) {
+        Element e = new Element();
+        e.data = d;
+
+        Element last = this.prev;
+
+        last.next = e;
+        e.next = this;
+        this.prev = e;
+        e.prev = last;
+    }
+
+    /// Liefert die Länge der Liste.
+    public int size() {
+        int count = 0;
+        Element current = this.next;
+
+        while (current != this) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+}
---
+++ src/main/java/algods/lecture/own/vl_2026_03_10/Lists/LinkedList.java
@@ -0,0 +1,5 @@
+package algods.lecture.own.vl_2026_03_10.Lists;
+
+public class LinkedList {
+    public Element anchor;
+}
